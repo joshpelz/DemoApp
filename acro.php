0e0ae35aa3a17dc58ca3b17735c8b59ff8792659
Josh P <joshpelz@users.noreply.github.com>
2016-07-13 08:22:46 -0700

    Update acro.php

--- acro.php
+++ acro.php
@@ -2,27 +2,33 @@
 
 require 'vendor/autoload.php';
 
-if(isset($_POST['acro'])) {
+function failed($error) {
+        // custom error code
+        echo "The request was not a valid acronym, or no results were found.";
+        echo "Error output:";
+        echo $error."<br /><br /><br />";
+        die();
+}
 
+if(!isset($_POST['acro'])) {
+	 failed('No valid acronym was submitted, perhaps the field was left blank!');
+}
 
-        function failed($error) {
-                // custom error code
-                echo "The request was not a valid acronym, or no results were found.";
-                echo "Error output:";
-                echo $error."<br /><br /><br />";
-                die();
-        }
+$url = 'http://www.nactem.ac.uk/software/acromine/dictionary.py';
+$acro = $_POST['acro']; // required
+echo "<br />Requested Acronym:";
+echo $acro;
 
-        if(!isset($_POST['acro'])) {
-		 failed('No valid acronym was submitted, perhaps the field was left blank!');
-	}
-
-        $req = $_POST['acro']; // required
-	echo "<br />Requested Acronym:";
-	echo $req;
+$req = 'http://www.nactem.ac.uk/software/acromine/dictionary.py'
+$req .= '?sf='
+$req .= $acro
+$req .= '?lf='
+$send = curl_init($req);
+$result = curl_exec($send)
+echo json_decode($result)
 
         $client = new GuzzleHttp\Client();    // init Guzzle client
-        $acros = $client->get('http://www.nactem.ac.uk/software/acromine/dictionary.py', array('sf' => $req)); // encode request parameters and submit to Acromine API
+        $acros = $client->get('http://www.nactem.ac.uk/software/acromine/dictionary.py', array('sf' => $acro)); // encode request parameters and submit to Acromine API
         echo "<br />Response Code:<br />";
 	echo $acros->getStatusCode();                    // response 200
         echo "<br />Encoding:<br />";
